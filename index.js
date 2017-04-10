90e615e0cbe32df3d5dd147d1fb081e038502b60
Cora Johnson-Roberson <cora.johnson.roberson@gmail.com>
2017-04-09 23:37:01 -0400

    try the main hook multiple times

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ var utils = require("./lib/utils");
 var ui = require("./lib/ui");
 var zotero = require("./lib/zotero");
 var exporter = require("./lib/exporter");
+var {
+  setTimeout
+} = require("sdk/timers");
 
-var logger = new utils.Logger(zotero.getZotero());
 
 function main(options, callbacks) {
+  var Zotero = zotero.getZotero();
+  if (!Zotero) { // try again in 1 second
+    setTimeout(main, 1000);
+    return;
+  }
+
+  var logger = new utils.Logger(Zotero);
   logger.info("zotero-voyant-export loaded.");
   ui.insertExportMenuItem(exporter.doExport);
 }
